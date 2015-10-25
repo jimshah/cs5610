3c93a99d16a8d7accce5db8c4ebeecdc0d895a0a
Jainam Shah <jimshah@ccs.neu.edu>
2015-10-25 15:03:01 -0400

    adding admin page

---
+++ public/assignment/admin/admin.controller.js
@@ -0,0 +1,39 @@
+(function(){
+	'use strict';
+
+	//Declaring sub module
+	var moduleName = "admin";
+
+	// Use app's registerModule function to register a new module
+	myFormApp.registerModule(moduleName);
+
+	//Defining header controller
+	angular
+	.module(moduleName)
+	.controller("AdminController", ['$scope', '$location', '$rootScope', 'UserService', AdminController]);
+	
+	//HeaderController function
+	function AdminController($scope, $location, $rootScope, UserService){
+		$scope.$location = $location;
+		$scope.user = $rootScope.user;
+
+		//listen for login/sigin to grab logged in user
+		$rootScope.$on("auth", function(event, user){
+			$scope.user = $rootScope.user = user;
+		});
+
+		var init = function(){
+			$scope.error = null;
+			if ($scope.user){
+				UserService.findAllUsers(function(error, allUsers){
+					$scope.allUsers = allUsers;
+				});
+			} else {
+				$scope.error = "please login to view this content";
+			}
+		};
+		init();
+		
+	};
+
+})();
